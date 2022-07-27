04c1b1f589b15496518db6ba302fa83d8cc5cbee
Andries Felix <56032612+efix66@users.noreply.github.com>
2022-07-27 15:23:18 +0300

    Test Commit

--- hooks-tutorial/src/useContext/UseContext4/UserContext2.js
+++ hooks-tutorial/src/useContext/UseContext4/UserContext2.js
@@ -3,42 +3,42 @@ import React, { createContext, useContext, useState, useEffect } from "react";
 const TheUserName = createContext("Unknown");
 
 const UserContext2 = () => {
-	const [userName, setUserName] = useState("Andries Felix");
+  const [userName, setUserName] = useState("Andries Felix");
 
-	useEffect(() => {
-		setInterval(() => {
-			setUserName("Felix, Andries Felix");
-		}, 2000);
-	}, []);
+  useEffect(() => {
+    setInterval(() => {
+      setUserName("Felix, Andries Felix");
+    }, 2000);
+  }, []);
 
-	return (
-		<TheUserName.Provider value={userName}>
-			The User Name is :
-			<Layout />
-		</TheUserName.Provider>
-	);
+  return (
+    <TheUserName.Provider value={userName}>
+      The User Name is :
+      <Layout />
+    </TheUserName.Provider>
+  );
 };
 
 const Layout = ({ children }) => {
-	return (
-		<div>
-			{children}
-			<Header />
-		</div>
-	);
+  return (
+    <div>
+      {children}
+      <Header />
+    </div>
+  );
 };
 
 const Header = () => {
-	return (
-		<header>
-			<UserInfo />
-		</header>
-	);
+  return (
+    <header>
+      <UserInfo />
+    </header>
+  );
 };
 
 const UserInfo = () => {
-	const userName = useContext(TheUserName);
-	return <h1>{userName}</h1>;
+  const userName = useContext(TheUserName);
+  return <h1>{userName}</h1>;
 };
 
 export default UserContext2;
